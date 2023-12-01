187fafe1c757b4f53fd0110d7572ca718786d0fa
Matias Morales <matias.andres.mh@gmail.com>
2023-11-30 23:53:18 -0300

    ejercicio de multiplicación node encapsulado en un arrow funtion

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 // tabla del 5
 
+multi = (tabla)=>{
 
-let number = 5;
+    let number = 12;
 
-for (let index = 1; index <= number; index++) {
+    for (let index = 1; index <= number; index++) {
+        
+
+        console.log(` ${tabla} x ${index}` + ` = ${tabla*index}` );
     
-    console.log('5 x '$ );
+        
+    }
 
-    
 }
+
+multi(355);
+
