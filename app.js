d658e9bff921173524fec985ae02bc550a557d69
Matias Morales <matias.andres.mh@gmail.com>
2023-12-01 00:06:33 -0300

    Arreglos en la visual de clg

--- app.js
+++ app.js
@@ -2,15 +2,21 @@
 
 multi = (tabla)=>{
 
+    console.clear();
+    console.log("===============================");
+    console.log(`============  ${tabla } ================`);
+    console.log("===============================");
+    console.log("");
+
     let number = 12;
 
     for (let index = 1; index <= number; index++) {
         
 
         console.log(` ${tabla} x ${index}` + ` = ${tabla*index}` );
-    
-        
     }
+    console.log(""); 
+    console.log("===============================");
 
 }
 
