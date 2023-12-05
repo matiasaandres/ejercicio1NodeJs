61d38f6bed758b092e2288acd6c5cb513d14542c
Matias Morales <matias.andres.mh@gmail.com>
2023-12-05 15:12:34 -0300

    rey cactch en multiplicación

--- helpers/multiplicar.js
+++ helpers/multiplicar.js
@@ -1,24 +1,34 @@
 const fs = require('fs'); 
 
+
+try {
+    const  multiplicacion = async(tabla) => {
+        console.clear();
+        console.log("===============================");
+        console.log(`============  ${tabla} ================`);
+        console.log("===============================");
+        console.log("");
+      
+        let number = 12;
+        let salida = "";
+      
+        for (let index = 1; index <= number; index++) {
+          salida = salida + ` ${tabla} x ${index}` + ` = ${tabla * index} \n`;
+        }
+      
+        // file System para crear un txt
+      fs.writeFileSync(`tabla-${tabla}.txt`,salida);
+
+      return `tabla-${tabla}`;
+
+    }
+
+} catch (err) {
+    throw err;
+}
+
  
-const  multiplicacion = async(tabla) => {
-  console.clear();
-  console.log("===============================");
-  console.log(`============  ${tabla} ================`);
-  console.log("===============================");
-  console.log("");
 
-  let number = 12;
-  let salida = "";
-
-  for (let index = 1; index <= number; index++) {
-    salida = salida + ` ${tabla} x ${index}` + ` = ${tabla * index} \n`;
-  }
-
-  // file System para crear un txt
-fs.writeFileSync(`tabla-${tabla}.txt`,salida);
-console.log("Archivo Creado!!!");
-};
 
 // exportaciones de las funciones
 module.exports = {
